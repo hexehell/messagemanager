0a108b1788a3b28a7166c4c27f7e0bf2a26aa8aa
hexehell <hecor36@gmail.com>
2023-11-04 15:33:05 -0600

    t-pc trying to retrieve all the messages on the phone

--- src/Main.ts
+++ src/Main.ts
@@ -1,6 +1,6 @@
-import { Client,LocalAuth } from "whatsapp-web.js"
+import WAWebJS, { Client,LocalAuth } from "whatsapp-web.js"
 
-const main = ()=>{
+const main = async()=>{
 
     const qrcode = require('qrcode-terminal');
 
@@ -11,11 +11,47 @@ const main = ()=>{
         qrcode.generate(qr, {small: true});
     });
     
-    client.on('ready', () => {
+    client.on('ready', async() => {
         console.log('Client is ready!');
 
-        console.log(client.getChats())
+        const chatContactsMessages:unknown[] = []
 
+        const chats:WAWebJS.Chat[] = await client.getChats()
+
+        const chatsNotContacts:WAWebJS.Chat[] = chats.filter(chat=>!chat.isGroup && chat.name.startsWith('+'))
+        const chatsContacts:WAWebJS.Chat[] = chats.filter(chat=>!chat.isGroup && !chat.name.startsWith('+'))
+
+        const waitForFetching:Promise<unknown>[] = []
+
+        chatsContacts.forEach(async(chat:WAWebJS.Chat) => {
+            
+          waitForFetching.push(
+            new Promise(async (resolve)=>{
+
+                chatContactsMessages.push({
+                    number:chat.id.user,
+                    name:chat.name,
+                    messages:await chat.fetchMessages({limit:1024})
+                    // messagesFromMe: await chat.fetchMessages({fromMe:true,limit:Infinity}),
+                    // messagesNotFromMe: await chat.fetchMessages({fromMe:false,limit:Infinity})
+                })
+    
+                resolve(1)
+
+            })
+
+
+          ) 
+
+
+            
+
+        });
+
+       await Promise.all(waitForFetching)
+
+
+        console.log(chatContactsMessages)
 
     });
     
